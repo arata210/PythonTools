1fc98f7d608169b3db59341d229199cbe17e8fe3
Xroo <89401390+arata210@users.noreply.github.com>
2024-11-01 07:36:13 +0800

    fix spotify_canvas_downloader.py

--- spotify_canvas_downloader.py
+++ spotify_canvas_downloader.py
@@ -3,6 +3,33 @@ import json
 import urllib.request
 import os
 import argparse
+from bs4 import BeautifulSoup
+
+
+def spotify_auth(url, sp_dc):
+    cookies = {'sp_dc': sp_dc}
+    re = requests.get(url, cookies=cookies)
+    soup = BeautifulSoup(re.text, 'html.parser')
+
+    # 查找包含accessToken的script标签
+    session = soup.find(id='session')
+
+    # 提取accessToken
+    access_token = None
+    for script in session:
+        try:
+            # 转换为字典
+            data = json.loads(script.string)
+            if 'accessToken' in data:
+                access_token = data['accessToken']
+                break
+        except (ValueError, TypeError):
+            continue
+
+    if access_token:
+        return access_token
+    else:
+        return False
 
 
 def download_canvas(authorization, trackId):
@@ -22,10 +49,10 @@ def download_canvas(authorization, trackId):
     }
 
     re = requests.get('https://api-partner.spotify.com/pathfinder/v1/query', params=params, headers=headers)
-
     try:
         url = json.loads(re.text)['data']['trackUnion']['canvas']['url']
         name = url.split('/')[-1]
+        print(name)
 
         if not os.path.exists(name):
             urllib.request.urlretrieve(url, name)
@@ -34,19 +61,24 @@ def download_canvas(authorization, trackId):
             print(f"文件已存在.")
     except:
         print("存在未知错误.")
+        # print("未登录Cookies/文件路径错误/该音乐暂无Canvas/文件无法正常下载/链接错误")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='下载Spotify的Canvas.')
-    # parser.add_argument('trackId', type=str, help='输入歌曲的id.')
+    parser = argparse.ArgumentParser(prog='Spotify Canvas Downloader', description='下载Spotify的Canvas.')
     parser.add_argument('url', type=str, help='输入歌曲的分享链接.')
-    parser.add_argument('-auth', type=str, help='输入Authorization.（1小时过期）')
+    # parser.add_argument('-o', type=str, help='输出目录.')
+    parser.add_argument('-cookie', type=str, help='Cookies中sp_dc的值.')
+    # parser.add_argument('-trackId', type=str, help='输入歌曲的id.')
+    # parser.add_argument('-auth', type=str, help='输入Authorization.（1小时过期）')
 
     args = parser.parse_args()
-    url = args.url.split('/')[-1].split('?')[0]
-    # if args.trackId is None:
-    download_canvas(args.auth, url)
-    # elif args.url is None:
-    #     download_canvas(args.authorization, args.trackId)
-    # else:
-    #     print('缺少必填字段')
+    spotify_auth = spotify_auth(args.url, args.cookie)
+    if spotify_auth:
+        auth = 'Bearer ' + spotify_auth
+    else:
+        print('auth获取失败')
+    # print(auth)
+    trackId_from_url = args.url.split('/')[-1].split('?')[0]
+    print(trackId_from_url)
+    download_canvas(auth, trackId_from_url)
